daee9d3a11a408a1e99d956fc9e8d62a828c4ec1
Bojsza Réka <bojsza.reka.laura@gmail.com>
2025-11-30 22:22:42 +0100

    #14 Remaining work files

---
+++ Backend/NoteSharingAPI/class_library/DTO/NoteRequestModifyDTO.cs
@@ -0,0 +1,21 @@
+﻿using class_library.Enums;
+using class_library.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_library.DTO
+{
+	public class NoteRequestModifyDTO
+	{
+		public Guid ID { get; set; }
+		public Guid SubjectID { get; set; }
+		public string Topic { get; set; }
+		public string Description { get; set; }
+		public ERequestStatus Status { get; set; } = ERequestStatus.Requested;
+	}
+}
